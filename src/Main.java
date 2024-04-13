015c1ed6221c5ae8b0e8921a2507588a931ed86b
Александр <kozin.last@yandex.ru>
2024-04-13 12:34:58 +0300

    Исправил замечания!

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         System.out.println("Здача №1");
         int clientOS = 1;
         if (clientOS == 0) {
-            System.out.println("Установите версию приложения для Android по ссылке");
+            System.out.println("Установите версию приложения для IOS по ссылке");
         } else if (clientOS == 1) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            System.out.println("Установите версию приложения для Android по ссылке");
         } else {
             System.out.println("Приложение не установится");
         }
@@ -15,13 +15,13 @@ public class Main {
         System.out.println("Здача №2");
         int clientDeviceYear = 2012;
         if (clientOS == 0 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            System.out.println("Установите версию приложения для Android по ссылке");
         } else {
             System.out.println("Приложение не установится");
         }
@@ -37,18 +37,18 @@ public class Main {
         System.out.println("Задача №4");
         int deliveryDistance = 95;
         int deliveryTimeDay = 1;
-        if (deliveryDistance == 20) {
-            System.out.println("Потребуется дней" + deliveryTimeDay);
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + deliveryTimeDay);
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней" + (deliveryTimeDay + 1));
+            System.out.println("Потребуется дней: " + (deliveryTimeDay + 1));
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней" + (deliveryTimeDay + 2));
+            System.out.println("Потребуется дней: " + (deliveryTimeDay + 2));
         } else if (deliveryDistance > 100) {
             System.out.println("Доставки нет");
         }
 
         System.out.println("Задача №5");
-        char monthNumber = 12;
+        int monthNumber = 12;
         switch (monthNumber) {
             case 3,4,5 :
                 System.out.println("Весна");
